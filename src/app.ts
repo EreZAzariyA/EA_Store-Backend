0065e03f24988273e76bcb77193bfa98304b9e73
Erez <erezazariya@gmail.com>
2022-10-17 02:33:15 +0300

    port

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,7 @@ import cookieSession from "cookie-session";
 const expressServer = express();
 expressServer.use(cors());
 
-const port: number = +process.env.PORT;
-console.log(port);
+const port: number = +process.env.PORT || 5000;
 
 expressServer.use(express.json()); // Support json in the body
 expressServer.use(cookieSession({
