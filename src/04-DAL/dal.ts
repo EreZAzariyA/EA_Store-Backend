db14e111b169ba768b3f987e03cff3be1e163830
Erez <erezazariya@gmail.com>
2022-11-07 18:09:20 +0200

    test

--- src/04-DAL/dal.ts
+++ src/04-DAL/dal.ts
@@ -1,7 +1,7 @@
 import mysql, { MysqlError } from "mysql";
 
 const connection = mysql.createPool({
-      host: process.env.MYSQL_HOST || "ea-store.cajb38aisi5q.eu-central-1.rds.amazonaws.com",
+      host: process.env.MYSQL_HOST || "eastore-db.cajb38aisi5q.eu-central-1.rds.amazonaws.com",
       user: process.env.MYSQL_USER || "erezazariya",
       password: process.env.MYSQL_PASSWORD || "59713973",
       database: process.env.MYSQL_DATABASE || "eastore"
